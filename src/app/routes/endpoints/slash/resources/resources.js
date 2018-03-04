1bb834636bed5ca7d196ecbf29fa6423e12d320d
stain88 <marcbaghdadi@gmail.com>
2018-03-04 16:54:40 +0000

    post the default help message under /resources

--- src/app/routes/endpoints/slash/resources/resources.js
+++ src/app/routes/endpoints/slash/resources/resources.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const qs = require('querystring');
+const postResult = result => console.log(result.data);
 
 const helpData = require('routes/data/slash/resources/resourcesHelp.js').help;
 const incomingParser = require('util/incomingParser.js');
@@ -10,6 +11,26 @@ const resources = async (req, res) => {
   const parsedCommand = await incomingParser.parsePayload(req);
 
   console.log(parsedCommand);
-}
+
+  if (req.body.token === process.env.SLACK_VERIFICATION_TOKEN) {
+    switch (recipients.action) {
+      case 'list':
+        console.log("this is the list action");
+        break;
+      default:
+        helpMessage(helpData, recipients.target_channel_id);
+        res.sendStatus(200);
+    }
+  } else {
+    res.sendStatus(503);
+  }
+};
+
+const helpMessage = (helpData, target_channel_id) => {
+  helpData.channel = target_channel_id;
+  const params = qs.stringify(helpData);
+  const sendMessage = axios.post('https://slack.com/api/chat.postMessage', params);
+  sendMessage.then(postResult);
+};
 
 module.exports = { resources };
