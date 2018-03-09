1a2ac306d9bfd201196786fd2867ec8eb72b3d78
Bethany Garcia <bethanymgarcia@gmail.com>
2018-03-09 05:06:49 -0800

    Filled in and extended YAML file for resources. Adjusted template and added an extra field.

---
+++ src/app/routes/data/slash/resources/slashResources_test.js
@@ -0,0 +1,20 @@
+function makeMessage(strings, ...values){
+  return 'message';
+}
+
+let message = {
+      token: process.env.SLACK_TOKEN,
+      as_user: true,
+      link_names: true,
+      mrkdwn_in: ['text', 'pretext'],
+      attachments: JSON.stringify([
+        {
+          pretext: '${language-icon}  ${language-desc}\n\n\n'
+          title: '\n${level} ${language}\n\n',
+          text:  '${video-link}  ${video-desc}\n\n${tutorial-link}  ${tutorial-desc}\n\n${book-link}  ${book-desc}\n\n${class-link}  ${class-desc}\n\n${help_link}\n\n${more-questions}\n\n\n${maintainer}',
+          color: '${sidebar-color}',
+        }]),
+    };
+
+
+module.exports = { message };
