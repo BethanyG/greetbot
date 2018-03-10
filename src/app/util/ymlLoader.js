fc67319df06b01c04b3f2fff936db312501b752d
stain88 <marcbaghdadi@gmail.com>
2018-03-10 21:32:32 +0000

    Modify ymlLoader to glob all resources and message templates

--- src/app/util/ymlLoader.js
+++ src/app/util/ymlLoader.js
@@ -1,5 +1,21 @@
 const YAML = require('yamljs');
 const path = require('path');
-const resourcesData = YAML.load(path.join(__dirname, '..', 'routes', 'data', 'slash', 'resources', 'resourcesData.yml'));
+const glob = require('glob');
 
-module.exports = { resourcesData };
+const resources = []
+const messageTemplates = []
+// const resourcesData = YAML.load(path.join(__dirname, '..', 'routes', 'data', 'slash', 'resources', 'resourcesData.yml'));
+
+glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'data', 'resources') } => (err, files) {
+	files.forEach(file => {
+		resources.push(YAML.load(file));
+	});
+});
+
+glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'data', 'slash', 'resources', 'messageTemplates') } => (err, files) {
+	files.forEach(file => {
+		messageTemplates.push( { path.basename(file, '.yml'): YAML.load(file) } );
+	});
+});
+
+module.exports = { resources, messageTemplates };
