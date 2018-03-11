0c2621e0114006fafdf7d489ff3a519c60721468
stain88 <marcbaghdadi@gmail.com>
2018-03-11 09:38:47 +0000

    Add comments about glob

--- src/app/util/ymlLoader.js
+++ src/app/util/ymlLoader.js
@@ -4,17 +4,19 @@ const glob = require('glob');
 
 const resources = []
 const messageTemplates = []
-// const resourcesData = YAML.load(path.join(__dirname, '..', 'routes', 'data', 'slash', 'resources', 'resourcesData.yml'));
+
+// "cwd" sets the initial directory to start looking in
+// "absolute" returns an absolute path to the file, so YAML doesn't get confused
 glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'routes', 'data', 'resources'), 'absolute': true }, function (err, files) {
-	files.forEach(file => {
+  files.forEach(file => {
     resources.push(YAML.load(path.resolve(file)));
-	});
+  });
 });
 
 glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'routes', 'data', 'slash', 'resources', 'messageTemplates'), 'absolute': true }, function (err, files) {
-	files.forEach(file => {
-		messageTemplates.push( {[path.basename(file, '.yml')]: YAML.load(path.resolve(file)) } );
-	});
+  files.forEach(file => {
+    messageTemplates.push( {[path.basename(file, '.yml')]: YAML.load(path.resolve(file)) } );
+  });
 });
 
 module.exports = { resources, messageTemplates };
