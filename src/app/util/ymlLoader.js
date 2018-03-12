bdd8720d2abb9561fc2dd62b1e34bc701396c395
stain88 <marcbaghdadi@gmail.com>
2018-03-12 11:31:04 +0000

    Glob new paths

--- src/app/util/ymlLoader.js
+++ src/app/util/ymlLoader.js
@@ -2,21 +2,21 @@ const YAML = require('yamljs');
 const path = require('path');
 const glob = require('glob');
 
-const resources = []
-const messageTemplates = []
+const messageAttachments = []
+const messageBodies = []
 
 // "cwd" sets the initial directory to start looking in
 // "absolute" returns an absolute path to the file, so YAML doesn't get confused
-glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'routes', 'data', 'resources'), 'absolute': true }, function (err, files) {
+glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'routes', 'data', 'slash', 'resources', 'messageAttachments'), 'absolute': true }, function (err, files) {
   files.forEach(file => {
-    resources.push(YAML.load(path.resolve(file)));
+    messageAttachments.push(YAML.load(path.resolve(file)));
   });
 });
 
-glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'routes', 'data', 'slash', 'resources', 'messageTemplates'), 'absolute': true }, function (err, files) {
+glob("**/*.yml", { "cwd": path.join(__dirname, '..', 'routes', 'data', 'slash', 'resources', 'messageBodies'), 'absolute': true }, function (err, files) {
   files.forEach(file => {
-    messageTemplates.push( {[path.basename(file, '.yml')]: YAML.load(path.resolve(file)) } );
+    messageBodies.push( {[path.basename(file, '.yml')]: YAML.load(path.resolve(file)) } );
   });
 });
 
-module.exports = { resources, messageTemplates };
+module.exports = { messageAttachments, messageBodies };
