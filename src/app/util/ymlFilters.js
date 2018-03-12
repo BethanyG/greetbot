60d2c0cd600093d9781ccf42aa491c82bb33b297
stain88 <marcbaghdadi@gmail.com>
2018-03-12 16:48:30 +0000

    filter templates required

--- src/app/util/ymlFilters.js
+++ src/app/util/ymlFilters.js
@@ -13,7 +13,24 @@ const filterResources = (resourceList, filters) => {
 }
 
 const filterTemplates = (resources, messageTemplates) => {
-
+  let toKeep = {};
+  resources.forEach(resource => {
+    if (toKeep.hasOwnProperty(resource['language'])a2) {
+      if (!toKeep[resource['language']].includes(resource['level'])){
+        toKeep[resource['language']].push(resource['level']);
+      }
+    } else {
+      toKeep[resource['language']] = [resource['level']];
+    }
+  })
+  for (var template in messageTemplates) {
+    let language = template['language'].toLowerCase();
+    let level = template['level'].toLowerCase();
+    if (!(toKeep.hasOwnProperty(language) && toKeep[language].includes(level))) {
+      delete messageTemplates[template];
+    }
+  }
+  return messageTemplates;
 }
 
-module.exports = { filterResources };
+module.exports = { filterResources, filterTemplates };
