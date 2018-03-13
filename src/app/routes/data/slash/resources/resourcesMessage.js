00cdf3eaa07156ea5dfd64e1047ce4fdffb59a99
stain88 <marcbaghdadi@gmail.com>
2018-03-13 11:04:06 +0000

    Code cleanup, remove old duplicate templates, and move sortArguments to new util

--- src/app/routes/data/slash/resources/resourcesMessage.js
+++
@@ -1,9 +0,0 @@
-const message = {
-      token: process.env.SLACK_TOKEN,
-      as_user: true,
-      link_names: true,
-      mrkdwn_in: ['text', 'pretext'],
-    };
-
-
-module.exports = { message };
