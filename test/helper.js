4811fa144a5fbc7641aac6841803aed1c8691c79
stain88 <marcbaghdadi@gmail.com>
2018-03-15 23:35:48 +0000

    check default /welcome with no parameters posts to user as DM

--- test/helper.js
+++ test/helper.js
@@ -1,7 +1,11 @@
 const greetbot = require('../src/index.js');
 const user_id = process.env.SLACK_USER_ID;
+const user_dm_id = process.env.SLACK_DM_ID;
+const general_id = process.env.SLACK_GENERAL_ID;
 const slack_ver_token = process.env.SLACK_VERIFICATION_TOKEN;
 
 global.greetbot = greetbot;
 global.user_id = user_id;
+global.user_dm_id = user_dm_id;
+global.general_id = general_id;
 global.slack_ver_token = slack_ver_token;
--- test/welcome.test.js
+++ test/welcome.test.js
@@ -1,25 +1,30 @@
 const request = require('supertest');
 const chai = require('chai');
 const expect = chai.expect;
+const parsePayload = require('../src/app/util/incomingParser.js').parsePayload;
 
 describe('#POST /welcome', function() {
 
   describe('no arguments', function() {
 
     before(function setBody() {
-      body = {user_id: user_id, token: slack_ver_token};
+      req = {'body': {'command': '/welcome', 'text': '', 'user_id': user_id, 'token': slack_ver_token}};
     })
 
-    it('should be valid', function(done) {
-      body.command = '/welcome';
-      body.text = '';
+    it.skip('should be valid', function(done) {
       request(greetbot).post('/welcome')
-        .send(body)
+        .send(req.body)
         .end(function(err, res) {
           expect(res.statusCode).to.equal(200);
           done();
         });
     });
+
+    it('should target the user_id', async function() {
+      await parsePayload(req).then(res => {
+        expect(res.target_channel_id[0]).to.equal(user_dm_id);
+      });
+    })
 
   });
 });
