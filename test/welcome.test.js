7ec68b84769d46c44df8fff66a12485af90a698f
Angelo Cordon <angelocordon@gmail.com>
2018-04-15 17:56:14 -0700

    Skip welcome test
    Comment out test code based on some discussions regarding shutting off
    the `/welcome` command.

--- test/welcome.test.js
+++ test/welcome.test.js
@@ -1,24 +1,27 @@
-const request = require('supertest');
-const chai = require('chai');
-const expect = chai.expect;
-const parsePayload = require('../src/app/util/incomingParser.js').parsePayload;
-
-describe('#POST /welcome', function() {
-
-  describe('no arguments', function() {
-
-    before(function setBody() {
-      req = {'body': {'command': '/welcome', 'text': '', 'user_id': user_id, 'token': slack_ver_token}};
-    })
-
-    it.skip('should be valid', function(done) {
-      request(greetbot).post('/welcome')
-        .send(req.body)
-        .end(function(err, res) {
-          expect(res.statusCode).to.equal(200);
-          done();
-        });
-    });
-
-  });
-});
+// NOTE: Skipping welcome test as `/welcome` could be deprecated soon.
+// Angelo 4/18
+// const expect = require('chai').expect;
+// const request = require('supertest');
+// const greetbot = require('../src/index.js');
+//
+// describe.skip('#POST /welcome', function () {
+//   describe('no arguments', function () {
+//     const req = { 'body':
+//                   { 'command': '/welcome',
+//                     'text': ''
+//                     // 'user_id': user_id,
+//                     // 'token': slack_ver_token }
+//                   }
+//     };
+//
+//     it('should be valid', function (done) {
+//       request(greetbot).post('/welcome')
+//         .send(req.body)
+//         .end(function (err, res) {
+//           if (err) { console.log(err); }
+//           expect(res.statusCode).to.equal(200);
+//           done();
+//         });
+//     });
+//   });
+// });
