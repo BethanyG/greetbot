fcbdfae17d83743e65e8d7b2aecda7a11fd55a33
Marc Baghdadi <marcbaghdadi@gmail.com>
2018-04-24 05:20:32 +0100

    feat(resources): improve resources help message (#76)
    * feat(resources): improve resources help message
    
    * feat(resources): improve help wording for resources list command

--- src/app/routes/data/slash/resources/messageTemplates/resourcesHelp.js
+++ src/app/routes/data/slash/resources/messageTemplates/resourcesHelp.js
@@ -7,14 +7,14 @@ const help = {
      attachments: JSON.stringify([
        {
          title: '/resources is a command fetch recommended CodeBuddies resources on various topics.',
-         text: [ '* /resources : Prints this message.',
-                 '* /resources `list`: Prints a list of all available resources.',
-                 '* /resources `list`[resource name]: Prints a list of available topics for requested resource name.',
-                 '* /resources `[resource name]`: Sends _"getting started in [resource x]"_ message as a DM to the user who typed it.',
-                 '* /resources `[resource name] [level]`:  Sends _[resource x] [level y]_ message as a DM to the user who typed it.',
-                 '* /resources `[resource name] [level] @username`: Sends the result of the command (resource name, etc._) as a DM to the @user specified.',
-                 '* /resources `post`[resource name] [level] [#channel_name]: Sends a given resource message to the specified channel.',
-                 '* /resources `post`[resource name] [level] [#channel_name] @user: Sends a given resource message to a specified channel AND as a DM to the @user.'
+         text: [ '* /resources : Prints this message as a DM.',
+                 '* /resources `list`: Prints a list of all available resources, separated by language.',
+                 '* /resources `post [resource language]`: Sends _"getting started in [resource x]"_ message as a DM to the user who typed it.',
+                 '* /resources `post [resource language] [level]`:  Sends _[resource x] [level y]_ message as a DM to the user who typed it.',
+                 '* /resources `post [resource language] [level] @username`: Sends the result of the command (resource language, etc._) as a DM to the @user specified.',
+                 '* /resources `post [resource language] [level] [#channel_name]`: Sends a given resource message to the specified channel.',
+                 '* /resources `post [resource language] [level] [#channel_name] @user`: Sends a given resource message to a specified channel AND as a DM to the @user.',
+                 '* NOTE: * multiple filters, channels, and users can be used per request.'
                ].join('\n'),
          color: '#74c8ed',
        }]),
